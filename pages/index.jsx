753df450765a11053b47d2ab26ab34e36271e6d0
Matt Wilson <mattwwilson34@gmail.com>
2023-03-29 09:12:47 -0400

    build(project-setup): removed boilerplate, assets, and prettier

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Matt Wilson</title>
+        <meta name="description" content="Matt Wilson's Portfolio." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="my-class"></main>
+    </>
+  )
+}
