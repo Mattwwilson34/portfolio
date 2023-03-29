cfc512ab6061a45976660f3225d20d93c647fe47
Matt Wilson <mattwwilson34@gmail.com>
2023-03-29 10:26:34 -0400

    style(global): added scss variables to enforce global styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,7 +9,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="my-class"></main>
+      <main className="my-class">
+        <section>
+          <h1>My Portfolio</h1>
+          <h2>test</h2>
+        </section>
+      </main>
     </>
   )
 }
