bb985d69edbbccdeff012b29422c6669e3f012da
Matt Wilson <mattwwilson34@gmail.com>
2023-03-30 09:46:51 -0400

    feat(button): add project button to index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import Head from "next/head"
 import Navbar from "../components/navbar/Navbar"
 import Hero from "../components/hero/Hero"
 import SocialMediaMenu from "../components/social-media-menu/SocialMediaMenu"
+import Button from "../components/button/Button"
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
         <SocialMediaMenu />
         <section>
           <Hero />
+          <Button text='Projects'/>
         </section>
       </main>
     </>
