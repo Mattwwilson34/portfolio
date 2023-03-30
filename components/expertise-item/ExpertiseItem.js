0a6f9e88cf015c2734b22a95054e4b78f4f86607
Matt Wilson <mattwwilson34@gmail.com>
2023-03-30 14:36:11 -0400

    feat(expertise-item): add ExpertiseItem component

---
+++ components/expertise-item/ExpertiseItem.js
@@ -0,0 +1,16 @@
+import styles from "./expertise-item.module.scss"
+import Image from "next/image"
+
+function ExpertiseItem({ title, description, image }) {
+  return (
+    <div className={styles.expertiseItem}>
+      <div className={styles.header}>
+        <Image src={image} alt={title} width={60} />
+        <h3 className={styles.title}>{title}</h3>
+      </div>
+        <p className={styles.description}>{description}</p>
+    </div>
+  )
+}
+
+export default ExpertiseItem
