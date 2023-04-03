bf66696c4d0346a03190b51278d641d44847d2dd
Matt Wilson <mattwwilson34@gmail.com>
2023-04-03 17:28:46 -0400

    fix(button): add scroll feature to button component

--- components/button/Button.js
+++ components/button/Button.js
@@ -1,7 +1,17 @@
 import styles from "./button.module.scss"
 
+function scrollToSection(section) {
+  console.log("clicked")
+  document.querySelector(`#${section}`).scrollIntoView({
+    behavior: "smooth",
+  })
+  setTimeout(() => {
+    window.scrollBy(0, 1)
+  }, 800)
+}
+
 function Button({text}) {
-  return <button className={styles.button}>{text}</button>
+  return <button className={styles.button} onClick={()=> scrollToSection('projects')}>{text}</button>
 }
 
 export default Button
