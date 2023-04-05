a355951195378eb0e8b1831e23c338d7585aebbf
Matt Wilson <mattwwilson34@gmail.com>
2023-04-05 16:16:23 -0400

    chore(meta-tag): add og:url tag

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,6 +23,7 @@ export default function Home() {
           property="og:image"
           content="https://mattwwilson.com/og-image-thumbnail.png"
         />
+        <meta property="og:url" content="https://mattwwilson.com/" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
